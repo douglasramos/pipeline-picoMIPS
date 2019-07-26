043b6607702a2ea04d0b06abd8940dd834f19b3f
Pedro <pedrofelipebo@gmail.com>
2019-07-26 17:24:15 -0300

    instruções com nops funcionam

--- src/types.vhd
+++ src/types.vhd
@@ -9,18 +9,18 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-package types is		 
-	
+package types is
+
     subtype word_type     is std_logic_vector(31 downto 0);
 	type word_vector_type is array(natural range <>) of word_type;
-	
+
 	constant word_vector_init: word_type := (others => '0');
-	
+
 	constant word_vector_instruction1: word_type := x"8C410050";
 	constant word_vector_instruction2: word_type := x"20230005";
-	
+
 	constant word_vector_test: word_type := (others => '1');
-	
-	constant word_vector_instruction12: word_type :=(1 | 3 | 5 | 7 => '0', others => '1');
-	
+
+	constant word_vector_value: word_type := x"00000070";
+
 end package types;
